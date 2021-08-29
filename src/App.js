590dcea00c904f80348721be7bba6d1ea999556f
Tejaswi-Chaudhari <tejschaudhari522002@gmail.com>
2021-08-29 15:52:45 +0530

    tutorial done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [mode, setMode] = useState(false);
+  const switchMode = () => setMode(mode ? false : true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" data-theme={mode ? "dark" : "light"}>
+      <h1>Helloo!!</h1>
+      <h2>Let's begin with our tutorial!</h2>
+      <button onClick={switchMode}>Switch mode</button>
     </div>
   );
 }
